d711c66ba3aea55bbf9fa2421978800a0d06b9a1
Hao <74541600+haod98@users.noreply.github.com>
2022-08-10 21:23:25 +0200

    Save access token in localStorage

--- src/app/logged-in/logged-in.component.ts
+++ src/app/logged-in/logged-in.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface access_token {
+  access_token?: string;
+  refresh_token?: string;
+  expires_in?: string;
+}
 
 @Component({
   selector: 'app-logged-in',
   templateUrl: './logged-in.component.html',
-  styleUrls: ['./logged-in.component.scss']
+  styleUrls: ['./logged-in.component.scss'],
 })
 export class LoggedInComponent implements OnInit {
+  private token?: access_token;
+  public token_received = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) =>
+      params['pinterest'] ? (this.token = JSON.parse(params['pinterest'])) : ''
+    );
+    if (this.token) {
+      localStorage.setItem('access_token', JSON.stringify(this.token));
+      this.token_received = true;
+    }
   }
-
 }
