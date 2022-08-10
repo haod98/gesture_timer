c37f763fec8c92e915470f97e8e51f523c0c7732
Hao <74541600+haod98@users.noreply.github.com>
2022-08-10 20:12:04 +0200

    Login with pinterest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'gesture_timer';
+  redirectUrl = decodeURI(environment.app_url);
 }
